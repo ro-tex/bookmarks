565cb18c561aaf597a72dc255c4a15cad9734e49
Ivaylo Novakov <inovakov@gmail.com>
2017-02-20 20:53:12 +0200

    Added an edit form, enabled deletion.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,27 +1,55 @@
 class BookmarksController < ApplicationController
     
-    def index
+  def index
+    @bookmarks = Bookmark.all
+  end
+  
+  def new
+    @bookmark = Bookmark.new
+  end
+  
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    
+    if @bookmark.save
+      redirect_to bookmarks_url(@bookmark)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    if defined?(params) && !params[:format].nil?
+      @bookmark = Bookmark.find(params[:format])
+    else
       @bookmarks = Bookmark.all
+      render 'index'
     end
-    
-    def new
-      @bookmark = Bookmark.new
-    end
-    
-    def create
-      @bookmark = Bookmark.new(bookmark_params)
+  end
+  
+  def edit
+    @bookmark = Bookmark.find(params[:format])
+  end
+  
+  def update
+    @bookmark = Bookmark.find(params[:format])
       
-      if @bookmark.save
-        redirect_to bookmarks_url(@bookmark)
-      else
-        render 'new'
-      end
+    if @bookmark.update(bookmark_params)
+      redirect_to bookmarks_url(@bookmark)
+    else
+      render 'edit'
     end
-
-    private
-    
-    def bookmark_params
-      params.require(:bookmark).permit(:title, :url, :shortening)
-    end
-    
+  end
+  
+  def destroy
+    @bookmark = Bookmark.find(params[:format])
+    @bookmark.destroy
+    redirect_to bookmarks_url
+  end
+  
+  private
+  
+  def bookmark_params
+    params.require(:bookmark).permit(:title, :url, :shortening, :id)
+  end
 end
