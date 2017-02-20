4dc0e1ed16eaff4912e2b9c0b2f6bceec9184b4d
Ivaylo Novakov <inovakov@gmail.com>
2017-02-20 20:51:15 +0200

    Added a route that enables the bookmark.1 URL syntax. It needs to be before the other routes related to bookmarks.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
+  # We need this for the "bookmark.1" format.
+  # It also needs to be above 'resources :bookmarks'.
+  resource :bookmarks
+
   resources :sites
   resources :bookmarks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'bookmarks#index', as: 'home'
-  
 end
