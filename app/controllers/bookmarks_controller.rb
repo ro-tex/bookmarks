0ed081128815022ff17f1364b3d528324869262f
Ivaylo Novakov <inovakov@gmail.com>
2017-02-21 15:50:02 +0200

    Some whitespace control.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,23 +1,23 @@
 class BookmarksController < ApplicationController
-    
+
   def index
     @bookmarks = Bookmark.all
   end
-  
+
   def new
     @bookmark = Bookmark.new
   end
-  
+
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    
+
     if @bookmark.save
       redirect_to bookmarks_url(@bookmark)
     else
       render 'new'
     end
   end
-  
+
   def show
     if defined?(params) && !params[:format].nil?
       @bookmark = Bookmark.find(params[:format])
@@ -26,27 +26,27 @@ class BookmarksController < ApplicationController
       render 'index'
     end
   end
-  
+
   def edit
     @bookmark = Bookmark.find(params[:format])
   end
-  
+
   def update
     @bookmark = Bookmark.find(params[:format])
-      
+
     if @bookmark.update(bookmark_params)
       redirect_to bookmarks_url(@bookmark)
     else
       render 'edit'
     end
   end
-  
+
   def destroy
     @bookmark = Bookmark.find(params[:format])
     @bookmark.destroy
     redirect_to bookmarks_url
   end
-  
+
   def search
     unless params[:search].nil?
       # Preserve the search query in the search bar:
@@ -59,13 +59,13 @@ class BookmarksController < ApplicationController
       @bookmarks = Bookmark.where('url LIKE ANY (?) OR title LIKE ANY (?)', "{#{sql_search_pattern}}", "{#{sql_search_pattern}}")
     end
   end
-  
+
   private
-  
+
   def bookmark_params
     params.require(:bookmark).permit(:title, :url, :shortening, :id)
   end
-  
+
   def search_params
     params.require(:search).permit(:search_terms)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :sites
   resources :bookmarks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   root 'bookmarks#index', as: 'home'
   match 'search' => 'bookmarks#search', as: 'search', via: [:get, :post]
 end
